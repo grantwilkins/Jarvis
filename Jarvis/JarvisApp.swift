16129047fef313a6fa3bb2f1283f0e9491797974
Grant <grant.wilkins371@gmail.com>
2023-01-24 10:59:39 -0500

    init

---
+++ Jarvis/JarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisApp.swift
+//  Jarvis
+//
+//  Created by Grant Wilkins on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct JarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
