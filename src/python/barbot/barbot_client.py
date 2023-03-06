a8052c260e0439e28b4c43bde78765f711e386c0
Grant <grant.wilkins371@gmail.com>
2023-03-06 18:28:05 -0500

    Add certificates and permissions

--- src/python/barbot/barbot_client.py
+++ src/python/barbot/barbot_client.py
@@ -6,10 +6,13 @@ import grpc
 import barbot_pb2_grpc
 import barbot_pb2
 
+creds_path = '../../../certs/server-cert.pem'
 
 def run():
     print("Will try to place order ...")
-    with grpc.insecure_channel('0.0.0.0:50051') as channel:
+    with open(creds_path, 'rb') as f:
+        creds = grpc.ssl_channel_credentials(f.read())
+    with grpc.secure_channel('localhost:50051', creds) as channel:
         stub = barbot_pb2_grpc.BarbotStub(channel)
         response = stub.PlaceOrder(barbot_pb2.OrderRequest(user_id='gfwilki@clemson.edu', drink_num=3))
     print("Client ack received: " + response.ack)
