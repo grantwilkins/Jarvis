2c70781adb2695435c6a08dbd3fa070c2a0047cb
Grant <grant.wilkins371@gmail.com>
2023-03-15 11:19:59 -0400

    small changes

--- src/grpc/barbot_client.py
+++ src/grpc/barbot_client.py
@@ -6,21 +6,20 @@ import grpc
 import barbot_pb2_grpc
 import barbot_pb2
 
-creds_path = '../../certs/server-cert.pem'
-
 IP_ADDR = '0.0.0.0'
 PORT = '50051'
 
-def run():
+def place_order(user_id,drink_name,drink_id, stirring):
     print("Will try to place order ...")
-    with open(creds_path, 'rb') as f:
-        creds = grpc.ssl_channel_credentials(f.read())
     with grpc.insecure_channel(IP_ADDR + ':' + PORT) as channel:
         stub = barbot_pb2_grpc.BarbotStub(channel)
-        response = stub.PlaceOrder(barbot_pb2.OrderRequest(user_id='gfwilki@clemson.edu', drink_num=3))
-    print("Client ack received: " + response.ack)
-
+        response = stub.PlaceOrder(barbot_pb2.OrderRequest(
+            user_id    = user_id, 
+            drink_name = drink_name,
+            drink_id   = drink_id,
+            stirring   = stirring))
+    print("Client ack received: " + response.user_id + " ordered " + response.drink_name)
 
 if __name__ == '__main__':
     logging.basicConfig()
-    run()
+    place_order()
--- src/grpc/barbot_server.py
+++ src/grpc/barbot_server.py
@@ -4,10 +4,6 @@ import logging
 import grpc
 import barbot_pb2_grpc
 import barbot_pb2
-import hello
-
-private_key_path = "../../certs/server-key.pem"
-certificate_path = "../../certs/server-cert.pem"
 
 class Barbot(barbot_pb2_grpc.BarbotServicer):
 
@@ -19,11 +15,6 @@ def serve():
     port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     barbot_pb2_grpc.add_BarbotServicer_to_server(Barbot(), server)
-    with open(private_key_path, 'rb') as f:
-        private_key = f.read()
-    with open(certificate_path, 'rb') as f:
-        certificate_chain = f.read()
-    server_credentials = grpc.ssl_server_credentials( ( (private_key, certificate_chain), ) )
     server.add_insecure_port('0.0.0.0:' + port)
     server.start()
     print("Server started, listening on " + port)
